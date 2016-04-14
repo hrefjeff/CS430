0604cfea5b0931d30a66b311c6f27a971cd5d506
Jeffrey Allen <jeffreyallen92@gmail.com>
2016-04-13 23:49:23 -0500

    Final cs430 push

--- assign_3/program.cpp
+++ assign_3/program.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
 struct item
@@ -8,46 +10,46 @@ struct item
     int value;
 };
 
-vector<item> inventory;
-vector<item> bag;
-
-
-void print_item(item);
-void print_contents_of(vector<item> container);
 void insert_item_into(vector<item>& container, int weight, int value);
-void transfer_from_to(vector<item>& from_cont, vector<item>& to_cont);
-//vector<item> optimize(vector<item>& container, int max_weight, int current_value);
-
+int optimal_knapsack(vector<item>& container, int max_weight, int num_of_items);
+void print_contents_of(vector<item> container);
+void print_item(item);
+void readFile( string filename, 
+               vector<item> & container,
+               int & container_capacity,
+               int & num_of_items
+             );
 int main()
 {
 
-    const int num_of_items = 3;
-    const int max_weight = 50;
+    vector<item> inventory;
+    int max_weight;
+    int num_of_items;
 
-    // vector<int> columns(num_of_items, 0);
-    // vector<vector<int> > rows(3,num_of_items);
+    //=============Begin File Read Stuff=====================
+    fstream file;
+    string filename;
+    bool badFile = true;
 
-    // for(int i=0;i<rows.size(); i++)
-    // {
-    //     for (int j=0;j<rows[i].size(); j++)
-    //     {
-    //         cout << rows[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // transfer_from_to(inventory,bag);
+    do {
 
-    // print_contents_of(inventory);
-    // print_contents_of(bag);
+        cout << "Input file name: ";
+        cin >> filename;
+        
+        file.open(filename);
+        badFile = file.fail();
+        file.close();
 
-    insert_item_into(inventory,1,60);
-    insert_item_into(inventory,2,100);
-    insert_item_into(inventory,3,120);
+    } while (badFile);
 
-//    bag = optimize(inventory,50);
-    
+    readFile(filename,inventory,max_weight,num_of_items);
+    //=============End File Read Stuff=======================
+
     print_contents_of(inventory);
-    print_contents_of(bag);
+
+    int optimal_value = optimal_knapsack(inventory,max_weight,num_of_items);
+
+    cout <<  "Optimal Knapsack Value: " << optimal_value << endl;
 
     return 0;
 }
@@ -59,10 +61,16 @@ void print_item(item x)
 
 void print_contents_of(vector<item> container)
 {
+
+    cout << endl << "=====Contents of container=====" << endl;
+
     for (int i = 0; i < container.size(); i++)
     {
+        cout << "Item " << i << ": ";
         print_item(container[i]);
     }
+
+    cout << "==============================="<< endl;
 }
 
 void insert_item_into(vector<item>& container, int weight, int value)
@@ -72,33 +80,118 @@ void insert_item_into(vector<item>& container, int weight, int value)
     container[container.size() - 1].value = value;
 }
 
-void transfer_from_to(vector<item>& from_cont, vector<item>& to_cont)
+/*
+
+    3 items, max weight 5
+
+    a: (W: 1, V: 60)
+    b: (W: 2, V: 100)
+    c: (W: 3, V: 120)
+
+    5 | 0 | 60 | 180  | [220]
+    4 | 0 | 60 | 180  | 180
+    3 | 0 | 60 | 180  | 180
+    2 | 0 | 60 | [120]| 120
+    1 | 0 | 60 | 60   | 60
+    0 | 0 | 0  | 0    | 0 
+    ------------------------
+        0 | a  |  b   | c
+
+*/
+int optimal_knapsack(vector<item>& container, int max_weight, int num_of_items)
 {
-    item temp = from_cont[0];
-    from_cont.erase(from_cont.begin());
-    to_cont.push_back(temp);
+
+    int item, weight, value_with_item, value_without_item;
+    int optimal_table[num_of_items+1][max_weight+1];
+
+    // iterate through items
+    for (int item = 0; item <= num_of_items; item++)
+    {
+        // assign optimal value for wieght of item i
+        for (int weight = 0; weight <= max_weight; weight++)
+        {
+            // base case where no money can be made, 
+            // where no item is chosen
+            // carrying capacity of bag is 0
+            if (item == 0 || weight == 0)
+            {
+                //cout << "Assigning [" << item << "," << weight << "] : 0" << endl;
+                optimal_table[item][weight] = 0;
+            }
+
+            // if item fits in bag
+            else if (container[item-1].weight <= weight)
+            {
+                // add v_sub_i, subtract w_sub_i
+                value_with_item = container[item-1].value + optimal_table[item-1][weight - container[item-1].weight];
+                value_without_item = optimal_table[item-1][weight];
+
+                //assign bigger of the two values
+                if (value_with_item > value_without_item)
+                {
+                    //cout << "Assigning [" << item << "," << weight << "] : " << value_with_item << " (value including item #" << item-1 << ")" << endl;
+                    optimal_table[item][weight] = value_with_item; 
+                }
+                else
+                {
+                    //cout << "Assigning [" << item << "," << weight << "] : " << value_without_item << " (value excluding item #" << item-1 << ")" << endl;
+                    optimal_table[item][weight] = value_without_item;
+                }
+
+            }
+
+            // just use previous value
+            else
+            {
+                //cout << "Assigning [" << item << "," << weight << "] : " << optimal_table[item-1][weight] << " (because item #" << item-1 << " won't fit)" << endl;
+                optimal_table[item][weight] = optimal_table[item-1][weight];
+            }
+        }
+    }
+
+    return optimal_table[num_of_items][max_weight];
+
 }
 
-// pg 369 for algorithm,
-// show it has 2 properties: pg 424 & 425
+void readFile(string filename, vector<item>& container, int & container_capacity, int & num_of_items)
+{ 
+    ifstream inFile;
+    inFile.open(filename.c_str());
 
-// vector<item> optimize(vector<item>& container, int max_weight, int current_value)
-// {
-//     // choose item i from container
+    int weight;
+    int value;
 
-//     // subtract i weight from max
+    if (!inFile.fail())
+    {
 
-//     // add value i to current value
+        inFile >> container_capacity;
+        inFile >> num_of_items;
 
-//     // remove i from container
+        item tmp = item();
 
-//     // vector<item> bag = optimize(container, max_weight, current_value)
+        while (!inFile.eof())
+        {
+            inFile >> value;
+            inFile >> weight;
 
+            insert_item_into(container,weight,value);
+        }
+    }
 
-//     // compare bag to solution w/o item i
-// }
+    inFile.close();
+}
 
-// vector<item> find_max_value_for_weight(vector<item>& inventory, int max_weight, int num_of_items)
-// {
-    
-// }
+//======================Resources=======================
+
+// show problem has 2 properties: pg 424 & 425
+// pg 369 for algorithm
+
+// BOTTOM_UP_CUT_ROD(p,n), where p is price table and n is max size pg 366
+// let r[0...n] be a new array        result array for saving subprobs
+// r[0] = 0
+// for j = 1 to n
+//     q = -inf
+//     for i = 1 to j
+//         q = max(q, p[i] + r[j-i])
+//     r[j] = q
+// return r[n]
---
+++ assign_3/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void compare(vector<bool>, vector<bool>);
+
+int main()
+{
+
+    vector<bool> list;
+    vector<bool> other_list;
+
+    list.resize(3);
+    other_list.resize(3);
+
+    return 0;
+}
+
+void compare(vector<bool> list_one, vector<bool> list_two)
+{
+    
+}
